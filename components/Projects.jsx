709fd483e1562539504c7777a0e6fc1fa7c0b61f
Jagoda Kosewska <jagoda@Shidfars-MacBook-Pro.local>
2022-12-08 23:02:59 +0100

    change colors of projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -9,10 +9,10 @@ function Projects() {
         <div className='max-w-[1240px] mx-auto px-2 py-16'>
             <p className='py-4 uppercase text-xl tracking-widest text-[#6a8680] font-bold'>My Projects</p>
             <div className='grid md:grid-cols-2 gap-8'>
-                <div className='relative flex itmes-center justify-center h-auto w-auto shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-[#3c8475] to-[#b15ac8]'>
+                <div className='relative flex itmes-center justify-center h-auto w-auto shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-[#3c8475] to-[#c2dad9]'>
                     <Image className='border-l-2 border-r-2 border-b-2 group-hover:opacity-25' src='/../public/Portfolio.png' width='500' height='400'/>
                 </div>
-                <div className='relative flex itmes-center justify-center h-auto w-auto shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-[#3c8475] to-[#b15ac8]'>
+                <div className='relative flex itmes-center justify-center h-auto w-auto shadow-xl shadow-gray-500 rounded-xl p-4 group hover:bg-gradient-to-r from-[#3c8475] to-[#c2dad9]'>
                     <Image className='border-l-2 border-r-2 border-b-2 group-hover:opacity-25' src='/../public/Portfolio.png' width='500' height='400'/>
                 </div>
             </div>
