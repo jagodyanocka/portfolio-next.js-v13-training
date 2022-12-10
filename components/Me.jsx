4cbfd990e7c690ec29812744d7704fd5eb680a00
Jagoda Kosewska <jagoda@Shidfars-MacBook-Pro.local>
2022-12-10 23:45:38 +0100

    make the icons linking profiles

--- components/Me.jsx
+++ components/Me.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 import { AiFillLinkedin, AiOutlineGithub, AiOutlineMail } from 'react-icons/ai';
 
@@ -11,13 +12,19 @@ function Me() {
                 <p className='py-4 max-w-[70%] m-auto'> I am a fresh full stack developer with backend majoring in <span className='font-bold'>Java</span>. I have graduated 13-weeks long coding bootcamp at <span className='font-bold'> School of applied technology </span> in Stockholm. Since then I am continously learing and improving my skills. </p>
                 <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
                     <div className='rounded-full shadow-lg shadow-[#3c8475] p-2'>
+                        <Link href='https://github.com/jagodyanocka'>
                         <AiOutlineGithub size={40} className='text-black cursor-pointer'/>
+                        </Link>
                     </div>
                     <div className='rounded-full shadow-lg shadow-[#3c8475] p-2'>
+                        <Link href='https://www.linkedin.com/in/jagoda-kosewska-b854021b7/?locale=en_US'>
                         <AiFillLinkedin size={40} className='text-black cursor-pointer'/>
+                        </Link>
                     </div>
                     <div className='rounded-full shadow-lg shadow-[#3c8475] p-2'>
+                        <Link href='/#contact'>
                         <AiOutlineMail size={40} className='text-black cursor-pointer'/>
+                        </Link>
                     </div>
                 </div>
             </div>
